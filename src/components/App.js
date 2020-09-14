911779937d979594d1267dfc523c6a6f140f0d06
A L V A R O <alvarop3000@gmail.com>
2020-09-14 15:13:01 -0500

    add skeleton of files

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel'
 
-function App() {
-  return (
-    <div className="app">
-      <container />
-    </div>
-  );
+class App extends React.Component{
+  render() {
+    return (
+      <div className="app">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
 }
 
 export default App;
